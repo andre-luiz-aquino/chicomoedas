275b3c093e2e5cbd7dd288bf6c69c591d5916ad9
gabrielfarias7 <gbfaria77@gmail.com>
2024-07-10 10:48:56 -0300

    Gran finale, última atualização.

--- lib/views/historico_cotacao.dart
+++ lib/views/historico_cotacao.dart
@@ -8,7 +8,15 @@ import 'package:chicomoedas/views/login_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:http/http.dart' as http;
+import 'package:intl/intl.dart';
 import 'package:syncfusion_flutter_charts/charts.dart';
+
+class Moeda {
+  final String nome;
+  final String imagem;
+
+  Moeda(this.nome, this.imagem);
+}
 
 class HistoricoCotacao extends StatefulWidget {
   const HistoricoCotacao({Key? key}) : super(key: key);
@@ -28,8 +36,16 @@ class _HistoricoCotacaoState extends State<HistoricoCotacao> {
   late Usuario usuarioLogado;
   final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
   String _nomeUsuario = '';
+  Moeda _moedaSelecionada = moedas[0];
+  static List<Moeda> moedas = [
+    Moeda('Dólar', 'assets/usa.png'),
+    Moeda('Euro', 'assets/aud.jpeg'),
+    Moeda('Pesos', 'assets/argentina.jpeg'),
+  ];
 
   List<CotacaoData> _dolarValues = [];
+  List<CotacaoData> _euroValues = [];
+  List<CotacaoData> _pesosValues = [];
 
   @override
   void initState() {
@@ -50,14 +66,20 @@ class _HistoricoCotacaoState extends State<HistoricoCotacao> {
   }
 
   Future<void> _fetchAndSetCurrencyValues() async {
-    List<CotacaoData> values = await fetchCurrencyValues();
+    List<CotacaoData> dolarValues =
+        await fetchCurrencyValues('http://172.210.138.201:8000/items/dolar');
+    List<CotacaoData> euroValues =
+        await fetchCurrencyValues('http://172.210.138.201:8000/items/euro');
+    List<CotacaoData> pesosValues =
+        await fetchCurrencyValues('http://172.210.138.201:8000/items/peso');
     setState(() {
-      _dolarValues = values;
+      _dolarValues = dolarValues;
+      _euroValues = euroValues;
+      _pesosValues = pesosValues;
     });
   }
 
-  Future<List<CotacaoData>> fetchCurrencyValues() async {
-    String apiUrl = 'http://172.210.138.201:8000/items';
+  Future<List<CotacaoData>> fetchCurrencyValues(String apiUrl) async {
     List<CotacaoData> values = [];
 
     try {
@@ -65,7 +87,7 @@ class _HistoricoCotacaoState extends State<HistoricoCotacao> {
       if (response.statusCode == 200) {
         var data = jsonDecode(response.body) as List<dynamic>;
         values = data.map((item) {
-          double value = double.tryParse(item['valor'] ?? '0.0') ?? 0.0;
+          double value = double.tryParse(item['maxima'] ?? '0.0') ?? 0.0;
           DateTime date = DateTime.parse(item['data']);
           return CotacaoData(date, value);
         }).toList();
@@ -89,17 +111,40 @@ class _HistoricoCotacaoState extends State<HistoricoCotacao> {
     Navigator.pushAndRemoveUntil(
       context,
       MaterialPageRoute(builder: (context) => const LoginPage()),
-          (Route<dynamic> route) => false,
+      (Route<dynamic> route) => false,
     );
   }
 
-  List<CartesianSeries<CotacaoData, DateTime>> _createChartData() {
+  List<CartesianSeries<CotacaoData, DateTime>> _createChartData(Moeda moeda) {
+    List<CotacaoData> dataSource;
+    Color color;
+
+    switch (moeda.nome) {
+      case 'Dólar':
+        dataSource = _dolarValues;
+        color = Colors.blue;
+        break;
+      case 'Euro':
+        dataSource = _euroValues;
+        color = Colors.red;
+        break;
+      case 'Pesos':
+        dataSource = _pesosValues;
+        color = Colors.green;
+        break;
+      default:
+        dataSource = [];
+        color = Colors.black;
+        break;
+    }
+
     return [
       LineSeries<CotacaoData, DateTime>(
-        dataSource: _dolarValues,
+        dataSource: dataSource,
         xValueMapper: (CotacaoData data, _) => data.data,
         yValueMapper: (CotacaoData data, _) => data.valor,
-        color: Colors.blue,
+        color: color,
+        name: moeda.nome,
       ),
     ];
   }
@@ -147,32 +192,80 @@ class _HistoricoCotacaoState extends State<HistoricoCotacao> {
               ),
             ],
           ),
-          body: SingleChildScrollView(
-            child: Stack(
-              children: [
-                SizedBox(
-                  width: double.infinity,
-                  height: 750.h,
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Padding(
-                        padding: EdgeInsets.all(16.w),
-                        child: SizedBox(
-                          height: 300.h, // Altura definida para o gráfico
-                          child: SfCartesianChart(
-                            primaryXAxis: DateTimeAxis(),
-                            series: _createChartData(),
+          body: Column(
+            children: [
+              Expanded(
+                child: SingleChildScrollView(
+                  child: Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 16.w),
+                    child: Stack(
+                      children: [
+                        SizedBox(
+                          width: double.infinity,
+                          height: 750.h,
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Padding(
+                                padding: EdgeInsets.all(16.w),
+                                child: Container(
+                                  width: 150.w,
+                                  height: 60.h,
+                                  decoration: const BoxDecoration(
+                                    shape: BoxShape.circle,
+                                  ),
+                                  child: DropdownButton<Moeda>(
+                                    value: _moedaSelecionada,
+                                    onChanged: (Moeda? newValue) {
+                                      if (newValue != null) {
+                                        setState(() {
+                                          _moedaSelecionada = newValue;
+                                        });
+                                      }
+                                    },
+                                    items: moedas.map((Moeda moeda) {
+                                      return DropdownMenuItem<Moeda>(
+                                        value: moeda,
+                                        child: Row(
+                                          children: <Widget>[
+                                            Image.asset(
+                                              moeda.imagem,
+                                              width: 50.sp,
+                                              height: 50.sp,
+                                            ),
+                                            SizedBox(width: 10.w),
+                                            Text(
+                                              moeda.nome,
+                                              style: TextStyle(fontSize: 20.sp),
+                                            ),
+                                          ],
+                                        ),
+                                      );
+                                    }).toList(),
+                                  ),
+                                ),
+                              ),
+                              SizedBox(
+                                height: 30.h,
+                              ),
+                              SizedBox(
+                                height: 300.h,
+                                child: SfCartesianChart(
+                                  primaryXAxis: DateTimeAxis(),
+                                  series: _createChartData(_moedaSelecionada),
+                                ),
+                              ),
+                              SizedBox(height: 20.h),
+                            ],
                           ),
                         ),
-                      ),
-                      SizedBox(height: 20.h),
-                    ],
+                      ],
+                    ),
                   ),
                 ),
-              ],
-            ),
+              ),
+            ],
           ),
           drawer: Drawer(
             child: ListView(
@@ -200,7 +293,7 @@ class _HistoricoCotacaoState extends State<HistoricoCotacao> {
                           builder: (context) => const ConversorPage(),
                           settings: const RouteSettings(name: '/'),
                         ),
-                            (Route<dynamic> route) => false,
+                        (Route<dynamic> route) => false,
                       );
                     } else {
                       Navigator.pop(context);
@@ -226,12 +319,14 @@ class _HistoricoCotacaoState extends State<HistoricoCotacao> {
                 ListTile(
                   title: const Text('Histórico de Cotação'),
                   onTap: () {
-                    if (ModalRoute.of(context)?.settings.name != '/historico_cotacao') {
+                    if (ModalRoute.of(context)?.settings.name !=
+                        '/historico_cotacao') {
                       Navigator.push(
                         context,
                         MaterialPageRoute(
                           builder: (context) => const HistoricoCotacao(),
-                          settings: const RouteSettings(name: '/historico_cotacao'),
+                          settings:
+                              const RouteSettings(name: '/historico_cotacao'),
                         ),
                       );
                     } else {
